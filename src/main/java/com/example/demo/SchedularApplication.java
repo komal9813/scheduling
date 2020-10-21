a1116b252565eb075f0951c41a03de0732f4755b
komal <komal98@gmail.com>
2020-10-21 23:24:40 +0530

    first commit

---
+++ src/main/java/com/example/demo/SchedularApplication.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchedularApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchedularApplication.class, args);
+		System.out.println("Hello ......Application is deployed and running");
+	}
+
+}
